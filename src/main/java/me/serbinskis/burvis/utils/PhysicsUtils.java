0a478ab7ee221822a381380280acb5b2d3929db1
WobbyChip <serbinskis@gmail.com>
2024-12-25 00:27:50 +0200

    Update PhysicsUtils.java

--- src/main/java/me/serbinskis/burvis/utils/PhysicsUtils.java
+++ src/main/java/me/serbinskis/burvis/utils/PhysicsUtils.java
@@ -5,6 +5,8 @@ public class PhysicsUtils {
         float mass = density; //m (mass) = p (density) * V (volume 1m^2)
         float drag_coefficient = 1.05f; //Cube - 1.05
         float area = 1;
+
+        //https://www.omnicalculator.com/physics/terminal-velocity
         return (float) Math.sqrt((2 * mass * gravity) / (density * area * drag_coefficient));
     }
 }
